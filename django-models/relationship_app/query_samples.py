e8924caa47f791b27aa0d396eb166b67bc501177
Rop <ropkiprotich6@gmail.com>
2024-11-10 12:39:08 -0800

    Moving relationship_app/models.py  modification

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -3,7 +3,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 # 1. Query all books by a specific author
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    return author.books.all()
+    return Book.objects.filter(author=author)
 
 # 2. List all books in a library
 def list_books_in_library(library_name):
@@ -11,9 +11,6 @@ def list_books_in_library(library_name):
     return library.books.all()
 
 # 3. Retrieve the librarian for a library
-def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.librarian
 def get_librarian_for_library(library_name):
     # Retrieve the library object
     library = Library.objects.get(name=library_name)
