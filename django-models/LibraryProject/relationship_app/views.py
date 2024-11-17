549095ccf273f56ade45ed556b8733a562016ee6
Rop <ropkiprotich6@gmail.com>
2024-11-17 09:08:32 -0800

    moving library.html file to its right location

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+
 
 # Create your views here.
 
 
 
 from .models import Book
-from django.views.generic import DetailView
 from .models import Book
 from .models import Library
 
