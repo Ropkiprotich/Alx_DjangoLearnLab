9a64c96ad77c95c9fe5b0bdba7c51c8af20e2099
Rop <ropkiprotich6@gmail.com>
2024-11-17 10:02:55 -0800

    C:/Program Files/Git/urls.py update in relationship_app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,6 +5,8 @@ from .views import list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import register
 
+
+
 urlpatterns = [
     path('books/', list_books, name='list_books'),  # For the list_books function-based view
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
