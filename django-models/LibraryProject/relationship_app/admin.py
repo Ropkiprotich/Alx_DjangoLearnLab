d43ea014cd7d55437a631f9a64a8c38b5dc923c4
Rop <ropkiprotich6@gmail.com>
2024-11-17 12:31:56 -0800

    C:/Program Files/Git/updates in relationship_app

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import UserProfile
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role')  # Display the associated user and role in the admin list view
+    search_fields = ('user__username', 'role')  # Add search functionality by username and role
+    list_filter = ('role',)  # Add a filter by role
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,7 +4,15 @@ from django.contrib.auth.views import LoginView, LogoutView
 from relationship_app import views
 from .views import list_books, LibraryDetailView, register, BookDetailView
 from . import views
+from .views.admin_view import admin_view
+from .views.librarian_view import librarian_view
+from .views.member_view import member_view
 
+urlpatterns = [
+    path('admin-view/', admin_view, name='admin_view'),
+    path('librarian-view/', librarian_view, name='librarian_view'),
+    path('member-view/', member_view, name='member_view'),
+]
 
 
 urlpatterns = [
