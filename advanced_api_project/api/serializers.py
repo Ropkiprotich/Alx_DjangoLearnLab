b466c8393787ce6e325609b04cd97d0bca98716b
Rop <ropkiprotich6@gmail.com>
2025-01-02 03:25:34 -0800

    API app debugging

--- advanced_api_project/api/serializers.py
+++ advanced_api_project/api/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
-from .models import Book
+from .models import Author, Book
+from datetime import datetime
 
 class BookSerializer(serializers.ModelSerializer):
     class meta:
        model = Book
        fields = '__all__'
+
+    def validate_publication_year(self, value):
+        current_year = datetime.now().year
+        if value > current_year:
+            raise serializers.ValidationError("The publication year cannot be in the future.")
+        return value
+
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
+    class meta:
+        model = Author
+        field = [ 'name', 'books']
+
