e9c802f19e7dea512e708d40d04d68a8fd3ec535
Rop <ropkiprotich6@gmail.com>
2025-01-04 00:58:40 -0800

    API app debugging

--- advanced_api_project/api/urls.py
+++ advanced_api_project/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from . import views 
 
 urlpatterns = [
     # Example API endpoint for listing authors
--- advanced_api_project/api/views.py
+++ advanced_api_project/api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.generics import ListAPIView
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
+
+# View for listing all authors
+class AuthorListView(ListAPIView):
+    queryset = Author.objects.all()  # Query all authors
+    serializer_class = AuthorSerializer  # Use AuthorSerializer for serialization
+
+# View for listing all books
+class BookListView(ListAPIView):
+    queryset = Book.objects.all()  # Query all books
+    serializer_class = BookSerializer  # Use BookSerializer for serialization
