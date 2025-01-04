ad5713839d05e5fe93fe0ab8e09a4699c8dd1966
Rop <ropkiprotich6@gmail.com>
2025-01-04 05:48:18 -0800

    Bookshelf/Admin.py file update

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
 from .models import Book
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")  # Fields to display in the list view
@@ -8,3 +11,25 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ("publication_year",) 
 
 admin.site.register(Book, BookAdmin)
+
+
+class CustomUserAdmin(UserAdmin):
+    # Specify the fields to display in the admin panel
+    model = CustomUser
+    list_display = ('username', 'email', 'phone_number', 'date_of_birth', 'is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password', 'profile_picture')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'phone_number', 'date_of_birth')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'profile_picture', 'phone_number', 'date_of_birth', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+# Register the custom user model
+admin.site.register(CustomUser, CustomUserAdmin)
