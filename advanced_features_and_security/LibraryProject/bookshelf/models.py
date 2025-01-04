87abea6be0f6425c835779d5363d72600b53bd88
Rop <ropkiprotich6@gmail.com>
2025-01-04 05:42:29 -0800

    Bookshelf/Models.py file update

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -14,7 +14,7 @@ class Book(models.Model):
 class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    profile_photo = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
 
     def __str__(self):
         return f"{self.username} ({self.email})"
