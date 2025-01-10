3756748583d85d3c91dffbe346bcbec0403c8e00
Rop <ropkiprotich6@gmail.com>
2025-01-10 09:21:25 -0800

    Updating serializer.py file

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,19 +1,22 @@
 from rest_framework import serializers
 from .models import CustomUser
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework import generics, permissions
+from .serializers import UserSerializer
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture']
-        extra_kwargs = {'password': {'write_only': True}}
+        username = serializers.CharField(max_length=100)
+    email = serializers.EmailField()
+    bio = serializers.CharField(allow_blank=True, required=False)
 
+    
     def create(self, validated_data):
         user = CustomUser.objects.create_user(**validated_data)
         return user
-
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
-from rest_framework.response import Response
 
 class CustomObtainAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
@@ -21,10 +24,6 @@ class CustomObtainAuthToken(ObtainAuthToken):
         token = Token.objects.get(key=response.data['token'])
         user = token.user
         return Response({'token': token.key, 'user_id': user.pk, 'username': user.username})
-
-from rest_framework import generics, permissions
-from .models import CustomUser
-from .serializers import UserSerializer
 
 class UserProfileView(generics.RetrieveUpdateAPIView):
     queryset = CustomUser.objects.all()
