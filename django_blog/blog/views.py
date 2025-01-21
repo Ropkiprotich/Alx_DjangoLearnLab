27547fc16b7cdaa0b7947cfd5311bde74cc8c45f
Rop <ropkiprotich6@gmail.com>
2025-01-21 01:21:12 -0800

    blogapp templates and static files connection

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -19,3 +19,26 @@ def register(request):
 # Profile view
 def profile(request):
     return HttpResponse("<h1>Welcome to your Profile!</h1>")
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import CustomUserCreationForm
+
+# Registration view
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to the login page after successful registration
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+# Profile management view
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
